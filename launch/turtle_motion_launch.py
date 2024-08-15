cc2f0858ac45b3fc82ea5aa49f3e8386c198fb46
Rosaline <mshk2016@icloud.com>
2024-08-15 08:49:39 -0700

    Update turtle_motion_launch.py
    Update Version 4: Remove redundant turtle_spawner

--- launch/turtle_motion_launch.py
+++ launch/turtle_motion_launch.py
@@ -12,11 +12,6 @@ def generate_launch_description():
         output="own_log"
     )
 
-    turtle_spawner_node  = Node(
-        package="turtle_motion",
-        executable="turtle_spawner"
-    )
-
     turt_ctrl_node =Node(
         package = 'turtle_motion',
         executable = 'go_to_goal'
@@ -24,8 +19,6 @@ def generate_launch_description():
 
 
     ld.add_action(turtlesim_node)
-    ld.add_action(turtle_spawner_node)
     ld.add_action(turt_ctrl_node)
-    # ld.add_action(turtle_teleop_key)
 
     return ld
